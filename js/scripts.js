275bae468c8782f496740dd0fed38bcfb8eb8782
Nicholas Pfister <npfister13@hotmail.com>
2021-11-23 17:07:45 -0500

    menu and ordering page functionality

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// $(function(){
+//     $('.drink').change(function(){
+//         var opt = $(this).val();
+//         if (opt !== ''){
+//             $('#food').removeClass('offset-3');
+//             $('.food').removeClass("offset-3");
+//             $('#temp').show();
+//             $('.temp').show();
+//         }else{
+//             $('#food').addClass('offset-3');
+//             $('.food').addClass('offset-3');
+//             $('#temp').hide();
+//             $('.temp').hide();
+//         }
+//     });
+// })
